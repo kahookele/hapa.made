c0427c6fed8d703042a508730f4b63949b147417
Keli'i Kaho'okele <keliikahookele@Mac.lan>
2025-03-21 10:16:14 -1000

    changes to workshop

--- workshop/urls.py
+++ workshop/urls.py
@@ -6,5 +6,3 @@ urlpatterns = [
     path('book/<int:workshop_id>/', book_workshop, name='book_workshop'),  # ✅ This handles /workshops/book/ID/
     path('booking-success/', booking_success, name='booking_success'),  # Add this
 ]
-
-# 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
